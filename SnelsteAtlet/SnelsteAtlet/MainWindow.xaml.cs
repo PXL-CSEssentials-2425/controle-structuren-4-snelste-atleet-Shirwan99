43bb2f36d905277e0d24b1d84e429598565fd7fa
Shirwan Abdulrahman <abdulrahmanshirwan@gmail.com>
2024-11-23 17:39:30 +0100

    Panels Final

--- SnelsteAtlet/SnelsteAtlet/MainWindow.xaml.cs
+++ SnelsteAtlet/SnelsteAtlet/MainWindow.xaml.cs
@@ -16,9 +16,56 @@ namespace SnelsteAtlet
     /// </summary>
     public partial class MainWindow : Window
     {
+        string nameFastest;
+        int secondsFastest = 0;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void newButten_Click(object sender, RoutedEventArgs e)
+        {   
+                //int secondsCurrent;
+            bool isvalidNumber = int.TryParse(secondsTextBox.Text, out int secondsCurrent);
+            
+            //if (isvalidNumber == true)
+            if (isvalidNumber)
+            {
+                if (secondsFastest == 0 || secondsCurrent < secondsFastest)
+                {
+                    secondsFastest = secondsCurrent;
+                    nameFastest = nameTextBox.Text;
+                }
+            }
+        }
+
+        private void CloseButten_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void clearButton(object sender, RoutedEventArgs e)
+        {
+            nameTextBox.Clear();
+            secondsTextBox.Clear();
+            resultTextBox.Clear();
+
+            nameTextBox.Focus();
+        }
+
+        private void resultTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void fastestButton_Click(object sender, RoutedEventArgs e)
+        {
+            resultTextBox.Text = $"De snelste atleet is {nameFastest} met een tijd van {secondsFastest} seconden";
         }
     }
 }
